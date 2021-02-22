3b295cb908889277bfa0c89084756ba926a960d1
Shan X <65002881+covetedpirate007@users.noreply.github.com>
2021-02-22 14:47:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/teamxprojects")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/teamxprojects")]
+            "Report Bugs 😊", url="https://t.me/teamxsupportbot")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
